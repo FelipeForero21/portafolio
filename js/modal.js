9da389e353b091840a60ab9d17bd8d41bf604d26
FelipeForero21 <elipeforero21@gmail.com>
2024-03-17 21:05:57 -0500

    agregacion de semana 1, alberto

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,15 @@
-const btnAbrirModal = document.getElementById('btnAbrirModal');
-const modal = document.getElementById('modal');
-const cerrarModal = document.getElementById('cerrarModal');
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAbrirModal = document.getElementById('btnAbrirModal');
+    const modal = document.getElementById('modal');
+    const cerrarModal = document.getElementById('cerrarModal');
 
-btnAbrirModal.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+    if (btnAbrirModal && modal && cerrarModal) {
+        btnAbrirModal.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
 
-cerrarModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.style.display = 'none';
+        cerrarModal.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
     }
 });
