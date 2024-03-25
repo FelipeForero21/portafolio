566622b0ca668fdb8a55e1e204e40116ba0c3682
FelipeForero21 <elipeforero21@gmail.com>
2024-03-25 11:48:46 -0500

    semana 2

--- js/modal.js
+++ js/modal.js
@@ -19,3 +19,34 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAbrirModal = document.getElementById('btnAbrirModal');
+    const modal = document.getElementById('modal');
+    const cerrarModal = document.getElementById('cerrarModal');
+
+    if (btnAbrirModal && modal && cerrarModal) {
+        const tarjetas = document.querySelectorAll('.abrir-modal');
+        tarjetas.forEach(tarjeta => {
+            tarjeta.addEventListener('click', (event) => {
+                event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+                const contenidoId = tarjeta.getAttribute('data-contenido');
+                const contenidoModal = document.querySelector(contenidoId);
+                if (contenidoModal) {
+                    modal.innerHTML = contenidoModal.innerHTML;
+                    modal.style.display = 'block';
+                }
+            });
+        });
+
+        cerrarModal.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+});
